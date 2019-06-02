7b226d7b754f666d973926fbc23ba6aa6affed8a
Dominik Toczek <dominiktoczek@interia.pl>
2019-06-02 10:01:53 +0200

    First commit

---
+++ CarInfo/CarInfo/Models/HomeMenuItem.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CarInfo.Models
+{
+    public enum MenuItemType
+    {
+        Home,
+        Cars,
+        Routes,
+        Statistics,
+        About
+    }
+    public class HomeMenuItem
+    {
+        public MenuItemType Id { get; set; }
+
+        public string Title { get; set; }
+    }
+}
---
+++ CarInfo/CarInfo/Views/MainPage.xaml.cs
@@ -0,0 +1,60 @@
+﻿using CarInfo.Models;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace CarInfo.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class MainPage : MasterDetailPage
+    {
+        Dictionary<int, NavigationPage> MenuPages = new Dictionary<int, NavigationPage>();
+        public MainPage()
+        {
+            InitializeComponent();
+
+            MasterBehavior = MasterBehavior.Popover;
+
+            MenuPages.Add((int)MenuItemType.Home, (NavigationPage)Detail);
+        }
+
+        public async Task NavigateFromMenu(int id)
+        {
+            if (!MenuPages.ContainsKey(id))
+            {
+                switch (id)
+                {
+                    case (int)MenuItemType.Home:
+                        MenuPages.Add(id, new NavigationPage(new HomePage()));
+                        break;
+                    case (int)MenuItemType.Cars:
+                        MenuPages.Add(id, new NavigationPage(new CarsPage()));
+                        break;
+                    case (int)MenuItemType.Routes:
+                        MenuPages.Add(id, new NavigationPage(new RoutesPage()));
+                        break;
+                    case (int)MenuItemType.Statistics:
+                        MenuPages.Add(id, new NavigationPage(new StatisticsPage()));
+                        break;
+                    case (int)MenuItemType.About:
+                        MenuPages.Add(id, new NavigationPage(new AboutPage()));
+                        break;
+                }
+            }
+
+            var newPage = MenuPages[id];
+
+            if (newPage != null && Detail != newPage)
+            {
+                Detail = newPage;
+
+                if (Device.RuntimePlatform == Device.Android)
+                    await Task.Delay(100);
+
+                IsPresented = false;
+            }
+        }
+    }
+}
