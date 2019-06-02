9545ab630418bd603d2708793ffe066d9ffc8424
Dominik Toczek <dominiktoczek@interia.pl>
2019-06-02 10:58:44 +0200

    Add cars, routes models

---
+++ CarInfo/CarInfo/Models/Car.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CarInfo.Models
+{
+    public class Car
+    {
+        public int ID { get; set; }
+        public string Brand { get; set; }
+        public string Model { get; set; }
+        public string Engine { get; set; }
+        public int Mileage { get; set; }
+        public string FuelType { get; set; }
+        public int FuelTankSize { get; set; }
+    }
+}
---
+++ CarInfo/CarInfo/Models/Route.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CarInfo.Models
+{
+    public class Route
+    {
+        public int ID { get; set; }
+        public DateTime Duration { get; set; }
+        public string Distance { get; set; }
+        public int carID { get; set; }
+    }
+}
